5eeed395b0791a5f9b32ef904bb64d33fc9ea333
AgentPapicha <mydak2109@gmail.com>
2023-05-25 19:54:18 +0300

    Ñ Try push

---
+++ lesson4/Task5.py
@@ -0,0 +1,5 @@
+list_1 = [1, 2, 3]
+list_2 = [1, 2, 3, 4, 5]
+
+result = [(x, y) for x in list_1 for y in list_2]
+print(result)
---
+++ lesson4/the_lesson.py
@@ -0,0 +1,9 @@
+x = 10
+if x < 10:
+    result = True
+else:
+    result = False
+
+result = True if x < 10 else False
+
+print(result)
