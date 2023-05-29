4bf38a36fae227d240f7751f2c84b409a6e9042d
AgentPapicha <mydak2109@gmail.com>
2023-05-29 18:24:12 +0300

    Try to fix no code problem

---
+++ lesson5/Task1.py
@@ -0,0 +1,3 @@
+dic = dict(mm=1, cm=2, m=3)
+new_dic = {v: k for k, v in dic.items()}
+print(new_dic)
---
+++ lesson5/Testing_tasks.py
@@ -0,0 +1,9 @@
+#Task1
+
+# print (new_dic,type(new_dic))
+# dic2 = reversed(dic)
+# a = [['moskva', 495], ['piter', 812], ['penza', 8412]]
+# dict.__reversed__(a)
+# q = dict.fromkeys(['a', 'b', 'c'], 100)
+# q2 = reversed(q)
+
