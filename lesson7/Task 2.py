099a084322e2e5051a7d7ea57e238b3522c2e686
AgentPapicha <mydak2109@gmail.com>
2023-06-06 17:43:45 +0300

    Task 3 not mine

---
+++ lesson7/Task 2.py
@@ -0,0 +1,3 @@
+num_list = [1, 2, 3, 4, 5]
+func = map(lambda x: str(x), num_list)
+print(list(func))
---
+++ lesson7/Task1.py
@@ -0,0 +1,3 @@
+n = int(input('Enter a natural number: '))
+odd_or_even = lambda x: print('чётное') if (x % 2 == 0) else print('нечётное')
+odd_or_even(n)
