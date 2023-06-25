ca297a925341be894ee3d57593aaebb7c66e177c
AgentPapicha <xlolshara@mail.ru>
2023-06-25 14:12:20 +0300

    Task 2 rewrited with Alex code

--- lesson9/Task2.py
+++ lesson9/Task2.py
@@ -1,42 +1,52 @@
-class Auto:
-    brand = ''
-    age = 0
-    color = ''
-    mark = ''
-    weight = ''
+import time
 
-    def __init__(self, brand, age, color, mark, weight):
-        self.brand = brand
-        self.age = age
-        self.color = color
-        self.mark = mark
-        self.weight = weight
+from lesson9.Task1 import Auto
 
+
+class Car(Auto):
+
+    def __init__(self, brand: str, age: int, mark: str, max_speed: int, color: str = "red", weight: int = 2000):
+        super().__init__(brand, age, mark, color=color, weight=weight)
+        self.max_speed = max_speed
 
     def move(self):
-        print('move')
-
-    def stop(self):
-        print('stop')
-
-    def birthday(self):
-        Auto.age = +1
-
-
-
-
+        super(Car, self).move(self)
+        print(f"Max speed is {self.max_speed}")
 
 
 class Truck(Auto):
-    def __init__(self, brand, age, color, mark, weight, max_load):
-        super().__init__(brand, age, color, mark, weight)
+
+    def __init__(self, brand: str, age: int, mark: str, max_load: int, color: str = "white", weight: int = 10_000):
+        super().__init__(brand, age, mark, color=color, weight=weight)
         self.max_load = max_load
 
-class Car(Auto):
-    def __init__(self,brand, age, color, mark, weight):
-        super().__init__(brand, age, color, mark, weight)
+    def move(self):
+        print("Attention!")
+        super(Truck, self).move(self)
+
+    @staticmethod
+    def load():
+        time.sleep(1)
+        print("Load ...")
+        time.sleep(1)
 
 
+# TEST
+car1 = Car("Mercedes", 10, "C 230", 250, weight=2_500)
+car2 = Car("Tesla", 2, "Model S", 300, color="green")
 
-Auto.move(print)
-Auto.stop(print)
+truck1 = Truck("MAZ", 20, "", 250, weight=25_000)
+truck2 = Truck("KAMAZ", 15, "", 300, color="gray", weight=20_000)
+
+for instance in (car1, car2, truck1, truck2):
+    print(f"Instance: {instance}")
+
+    for name, value in instance:
+        print(f"Attribute '{name}' has value: {value}")
+
+    instance.move()
+
+    if isinstance(instance, Truck):
+        instance.load()
+
+    print("-" * 30)
