a972788f463076d359d8b314b7fc3722c061002e
AgentPapicha <xlolshara@mail.ru>
2023-06-25 13:50:54 +0300

    ls9 task1 fixed with Alex code

--- lesson9/Task1.py
+++ lesson9/Task1.py
@@ -1,25 +1,25 @@
 class Auto:
-    brand = ''
-    age = 0
-    color = ''
-    mark = ''
-    weight = ''
-
-    def __init__(self, brand: str, age: int, mark: str):
+    def __init__(self, brand: str, age: int, mark: str, color: str = "black", weight: int = 1000):
         self.brand = brand
         self.age = age
         self.mark = mark
+        self.color = color
+        self.weight = weight
 
+    def __iter__(self):
+        for attr_name, attr_value in self.__dict__.items():
+            yield attr_name, attr_value
+
+    @staticmethod
     def move(self):
         print('move')
 
+    @staticmethod
     def stop(self):
         print('stop')
 
     def birthday(self):
-        Auto.age = +1
-
-
+        self.age += 1
 
 Auto.move(print)
 Auto.stop(print)
