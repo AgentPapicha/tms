17a00d713eda211564ba5c83776b99e185bc34b9
AgentPapicha <xlolshara@mail.ru>
2023-07-03 17:53:22 +0300

    Task 1 is 100% complete

--- lesson12/Task1.py
+++ lesson12/Task1.py
@@ -1,10 +1,7 @@
 import re
 
-user_phone = "+375-29-7776655"
-
 regex_phone_pattern = r"(\++[\d]{1,3})-([\d]{2})-([\d]{5,7})"
-
-regex_phone = re.compile(regex_phone_pattern)
+regex_compiled = re.compile(regex_phone_pattern)
 
 
 def test_valid_phone_numbers(regex_compiled):
@@ -17,9 +14,38 @@ def test_valid_phone_numbers(regex_compiled):
     )
     for n in valid:
         res = regex_compiled.fullmatch(n)
-        assert res.group() == n
+        if res:
+            print(f"The phone number {n} is valid \n")
+        else:
+            print(f"The phone number {n} is not valid")
 
 
-result = re.fullmatch(user_phone)
+def test_invalid_phone_numbers(regex_compiled):
+    invalid = (
+        "",
+        "test12345test"
+        "375-29-7776655",
+        "+-29-7776655",
+        "+3a5-29-7776655",
+        "+3756-29-7776655",
+        "+375--7776655",
+        "+375-4-7776655",
+        "+375-444-7776655",
+        "+375-c4-7776655",
+        "+375-33-",
+        "+375-33-7",
+        "+375-33-7776",
+        "+375-33-77766554",
+        "+375-29-7776e55",
+    )
 
-print(result)
+    for n2 in invalid:
+        res = regex_compiled.fullmatch(n2)
+        if res:
+            print(f"The phone number {n2} is valid")
+        else:
+            print(f"The phone number {n2} is not valid \n")
+
+
+test_valid_phone_numbers(regex_compiled)
+test_invalid_phone_numbers(regex_compiled)
